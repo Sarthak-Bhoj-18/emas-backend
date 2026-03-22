3b1aa860e2b08cb5ba1843cb82cd0648a8f3f047
SarthakBhoj <bsarthak4u@gmail.com>
2026-03-22 22:30:26 +0530

    ADDED ACTUATOR

--- src/main/java/com/rscoe/emas/config/SecurityConfig.java
+++ src/main/java/com/rscoe/emas/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/actuator/mappings").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
